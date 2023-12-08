afd89ab668d50a29b31872b311c1ed37a4496bc4
Anna Tsepilova <virgo.tsav@gmail.com>
2023-12-08 20:39:37 +0200

    add task-01

--- src/1.ts
+++ src/1.ts
@@ -5,7 +5,11 @@
 */
 
 class Student {
-  constructor(public name: string, public age: number, public grade: string) {}
+  constructor(public name: string, public age: number, public grade: string) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
 }
 
 export {};
